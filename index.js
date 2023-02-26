65e0be5ef3d46aa47fa9d82ea00b61065b40f840
PatrickEinstein <mohammedola1234@gmail.com>
2023-02-26 15:35:24 +0100

    repaired

--- index.js
+++ index.js
@@ -7,17 +7,16 @@ import multer from "multer";
 import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
-import {fileURLToPath} from "url";
+import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
-import { createPost} from "./controllers/posts.js"
-import {register} from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { register } from "./controllers/auth.js";
 import { verifyToken } from "./middleware/auth.js";
-import User from "./models/User.js";
-import { Post } from "./models/post.js";
-import {users, posts} from "./data/index.js"
-
+// import User from "./models/User.js";
+// import { Post } from "./models/post.js";
+// import {users, posts} from "./data/index.js"
 
 /* CONFIGURATION */
 const __filename = fileURLToPath(import.meta.url);
@@ -26,45 +25,48 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
-app.use(bodyParser.json({limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({ limit : "30mb", extended: true}))
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
 
 const storage = multer.diskStorage({
-    destination : function (req, file, cb){
-        cb(null, "public/assets");
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname);
-    }
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
 });
 
-const upload = multer({ storage});
+const upload = multer({ storage });
 
 // ROUTES WITH FILES
 
-app.post("auth/register", upload.single("picture"), register)
-app.post("/post", verifyToken, upload.single("picture"), createPost );
+app.post("auth/register", upload.single("picture"), register);
+app.post("/post", verifyToken, upload.single("picture"), createPost);
 
 //ROUTES
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
-app.use("/posts", postRoutes)
-
+app.use("/posts", postRoutes);
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
-mongoose.connect(process.env.MONGO_URL, {
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlparser: true,
-    useUnifiedTopology : true,
-}).then( ( )=>{
-    app.listen(PORT, ( )=> console.log(`Server is listening on Port: ${PORT}`))
-// ADD DATA ONE TIME
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB connected successfully")
+    // ADD DATA ONE TIME
     // User.insertMany(users);
     // Post.insertMany(posts);
-}).catch( ( error) => console.log(`${error} did not connect`));  
+  })
+  .catch((error) => console.log(`${error} did not connect`));
+  app.listen(PORT, () => console.log(`Server is listening on Port: ${PORT}`));
--- models/post.js
+++ models/post.js
@@ -1,40 +1,35 @@
 import mongoose from "mongoose";
-const postSchema = mongoose.Schema({
+
+const postSchema = mongoose.Schema(
+  {
     userId: {
-        type: String,
-        required : true,
+      type: String,
+      required: true,
     },
-
     firstName: {
-        type: String,
-        required : true,
+      type: String,
+      required: true,
     },
-
     lastName: {
-        type: String,
-        required : true,
+      type: String,
+      required: true,
     },
-
-    loctaion : String,
-
+    location: String,
     description: String,
-
-    PicturePath: String,
-
-    userPicturePath : String,
-
+    picturePath: String,
+    userPicturePath: String,
     likes: {
-        type: Map,
-        of: Boolean,
+      type: Map,
+      of: Boolean,
     },
-
-    comments :{
-        type: Array,
-        default :[]
+    comments: {
+      type: Array,
+      default: [],
     },
-
-},
-    { timestamps : true}
+  },
+  { timestamps: true }
 );
 
-export const Post = mongoose.model( "Post", postSchema);
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
