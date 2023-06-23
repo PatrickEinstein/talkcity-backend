abe2c68c4663eabd2eea063047d26f1d8e052194
PatrickEinstein <mohammedola1234@gmail.com>
2023-06-23 09:32:08 -0400

    cors3

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
-mongoose.set('strictQuery', true)
+mongoose.set("strictQuery", true);
 
 /* FILE STORAGE */
 const storage = multer.diskStorage({
@@ -46,39 +46,39 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
-app.use(
-  cors({
-    origin: "*",
-    methods: "GET, POST, PATCH",
-    allowedHeaders: "Content-Type",
-    credentials: true,
-  })
-);
-// Do you want to skip the checking of the origin and grant authorization?
-//https://vote-verse.vercel.app
-// const skipTheCheckingOfOrigin = true;
-// const allowedOrigins = ["*"];
 // app.use(
 //   cors({
-//     origin: function (origin, callback) {
-//       // allow requests with no origin (like mobile apps or curl requests)
-//       // or allow all origines (skipTheCheckingOfOrigin === true)
-//       if (!origin || skipTheCheckingOfOrigin === true)
-//         return callback(null, true);
-
-//       // -1 means that the user's origin is not in the array allowedOrigins
-//       if (allowedOrigins.indexOf(origin) === -1) {
-//         var msg =
-//           "The CORS policy for this site does not " +
-//           "allow access from the specified Origin.";
-
-//         return callback(new Error(msg), false);
-//       }
-//       // origin is in the array allowedOrigins so authorization is granted
-//       return callback(null, true);
-//     },
+//     origin: "*",
+//     methods: "GET, POST, PATCH",
+//     allowedHeaders: "Content-Type",
+//     credentials: true,
 //   })
 // );
+// Do you want to skip the checking of the origin and grant authorization?
+// https://vote-verse.vercel.app
+const skipTheCheckingOfOrigin = true;
+const allowedOrigins = ["*"];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps or curl requests)
+      // or allow all origines (skipTheCheckingOfOrigin === true)
+      if (!origin || skipTheCheckingOfOrigin === true)
+        return callback(null, true);
+
+      // -1 means that the user's origin is not in the array allowedOrigins
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
+
+        return callback(new Error(msg), false);
+      }
+      // origin is in the array allowedOrigins so authorization is granted
+      return callback(null, true);
+    },
+  })
+);
 
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
