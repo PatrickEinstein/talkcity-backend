4d4bd6818d9427089da2cb966909f96d44c866df
PatrickEinstein <mohammedola1234@gmail.com>
2023-06-23 10:00:17 -0400

    register route fixed

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,9 @@
 import express from "express";
 import { login, register } from "../controllers/auth.js";
 
-
-
 const router = express.Router();
 
-export const LoginPage =router.post("/login", login);
-export const RegisterPage = router.post("/" , register)
+export const LoginPage = router.post("/login", login);
+export const RegisterPage = router.post("/register", register);
 
 export default router;
